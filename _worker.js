7a65d450d7971f394e47752a4ac216c1330eecaa
xiaojianfei <xiao.jf@cidi.ai>
2024-09-09 20:00:54 -0700

    重新排版了一次

--- _worker.js
+++ _worker.js
@@ -6,14 +6,15 @@ function getWildcardFromDomain(domain) {
 }
 
 export default {
-    async fetch(request, env) {
-      let url = new URL(request.url);
+  async fetch(request, env) {
+    let url = new URL(request.url);
+    if(url.pathname.startWith('/'){
       let fdomain = getWildcardFromDomain(url.hostname);
       url.hostname= fdomain+".naszx.us.kg";
       let new_request=new Request(url,request);
       return fetch(new_request);
-      
+    }else  
       // Otherwise, serve the static assets.
-      //return env.ASSETS.fetch(request);
-    }
-  };
+      return env.ASSETS.fetch(request);
+  }
+};
